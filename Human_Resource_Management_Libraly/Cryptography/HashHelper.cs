00f209ec2c79e2c84feb1ccb5bed96ec169c387d
ezralight@outlook.com.vn <vuphamhocvienmatma@gmail.com>
2021-04-04 14:54:14 +0700

    Add Hash by PBKDF2-SHA512

--- Human_Resource_Management_Libraly/Cryptography/HashHelper.cs
+++ Human_Resource_Management_Libraly/Cryptography/HashHelper.cs
@@ -1,31 +1,70 @@
-﻿using System;
+﻿using Org.BouncyCastle.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Human_Resource_Management_Libraly.Cryptography
 {
-    public class HashHelper
+    public static class HashHelper
     {
-        public void CreatePasswordHash_Single()
-        {
-            int iterations = 100000; // The number of times to encrypt the password - change this
-            int saltByteSize = 64; // the salt size - change this
-            int hashByteSize = 512; // the final hash - change this
+       
+            private const int SaltByteSize = 32;
+            private const int HashByteSize = 64; // to match the size of the PBKDF2-HMAC-SHA-512 hash 
+            private const int Pbkdf2Iterations = 1000000; //3 giây
+            private const int IterationIndex = 0;
+            private const int SaltIndex = 1;
+            private const int Pbkdf2Index = 2;
 
-          
+            private static readonly SecureRandom _cryptoRandom = new SecureRandom();
 
-            var password = "password"; // That's really secure! :)
+            public static string HashPassword(string password)
+            {            
+                byte[] salt = new byte[SaltByteSize];
+                _cryptoRandom.NextBytes(salt);    
+                
+                var hash = GetPbkdf2Bytes(password, salt, Pbkdf2Iterations, HashByteSize);
+                return Pbkdf2Iterations + ":" +
+                       Convert.ToBase64String(salt) + ":" +
+                       Convert.ToBase64String(hash);
+            }
 
-            byte[] saltBytes = PBKDF2Provider.CreateSalt(saltByteSize);
-            string saltString = Convert.ToBase64String(saltBytes);
+            public static bool ValidatePassword(string password, string correctHash)
+            {
+                char[] delimiter = { ':' };
+                var split = correctHash.Split(delimiter);
+                var iterations = Int32.Parse(split[IterationIndex]);
+                var salt = Convert.FromBase64String(split[SaltIndex]);
+                var hash = Convert.FromBase64String(split[Pbkdf2Index]);
 
-            string pwdHash = PBKDF2Provider.PBKDF2_Keccak_GetHash(password, saltString, iterations, hashByteSize);
+                var testHash = GetPbkdf2Bytes(password, salt, iterations, hash.Length);
+                return SlowEquals(hash, testHash);
+            }
 
-            var isValid = PBKDF2Provider.ValidatePassword(password, saltBytes, iterations, 
-                hashByteSize, Convert.FromBase64String(pwdHash));
+            private static bool SlowEquals(byte[] a, byte[] b)
+            {
+                var diff = (uint)a.Length ^ (uint)b.Length;
+                for (int i = 0; i < a.Length && i < b.Length; i++)
+                {
+                    diff |= (uint)(a[i] ^ b[i]);
+                }
+                return diff == 0;
+            }
 
-        }
+            private static byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+            {
+                Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(
+                    password:password, 
+                    salt:salt,
+                    iterations: iterations,
+                    HashAlgorithmName.SHA512);
+
+                pbkdf2.IterationCount = iterations;           
+                
+                return pbkdf2.GetBytes(outputBytes);
+            }
+        
     }
 }
--- Human_Resource_Management_Web/Controllers/HomeController.cs
+++ Human_Resource_Management_Web/Controllers/HomeController.cs
@@ -21,21 +21,10 @@ namespace Human_Resource_Management_Web.Controllers
 
         public IActionResult Index()
         {
-           
-                int iterations = 100000; // The number of times to encrypt the password - change this
-                int saltByteSize = 64; // the salt size - change this
-                int hashByteSize = 512; // the final hash - change this
+            string test = "a";
+            var aHash = HashHelper.HashPassword(test);
+            bool i = HashHelper.ValidatePassword(test, aHash);
 
-                string password = "password"; // That's really secure! :)
-
-                byte[] saltBytes = PBKDF2Provider.CreateSalt(saltByteSize);
-                string saltString = Convert.ToBase64String(saltBytes);
-
-                string pwdHash = PBKDF2Provider.PBKDF2_Keccak_GetHash(password, saltString, iterations, hashByteSize);
-                
-                bool isValid = PBKDF2Provider.ValidatePassword(password, saltBytes, iterations,
-                    hashByteSize, Convert.FromBase64String(pwdHash));
-         
             return View();
         }
 
