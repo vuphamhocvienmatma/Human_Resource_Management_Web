d8895acb4570975ed5beebe469aef46d81892724
ezralight@outlook.com.vn <vuphamhocvienmatma@gmail.com>
2021-04-05 15:39:00 +0700

    Update _layout

---
+++ Human_Resource_Management_Web/Components/SidebarFooter/SidebarFooterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Human_Resource_Management_Web.Components.SidebarFooter
+{
+    [ViewComponent(Name = "SidebarFooterViewComponent")]
+    public class SidebarFooterViewComponent : ViewComponent
+    {
+        private IHttpContextAccessor _HttpContextAccessor;
+        public SidebarFooterViewComponent(IHttpContextAccessor HttpContextAccessor)
+        {
+            _HttpContextAccessor = HttpContextAccessor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Run(() => View());
+        }
+    }
+}
